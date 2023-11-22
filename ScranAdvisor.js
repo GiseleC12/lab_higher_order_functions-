0ab1d8f6444eeb6385e6554130ac2685793a89f3
Gisele <giselec12@Giseles-MacBook-Air.local>
2023-11-22 16:47:07 +0000

    Created a method for finding a restaurant by name

--- ScranAdvisor.js
+++ ScranAdvisor.js
@@ -3,8 +3,25 @@ const ScranAdvisor = function(restaurants){
 }
 
 // Add methods to prototype
+//Count the number of restaurants that were stored
 ScranAdvisor.prototype.numberOfRestaurants = function(){
     return this.restaurants.length;
 };
+
+//Find a restaurant by its name(full name)
+ScranAdvisor.prototype.findByName = function (name){
+    return this.restaurants.find(restaurant => restaurant.name === name );
+};
+
+
+//Return an array containing each restaurant's name 
+ScranAdvisor.prototype.getEachName = function (){
+
+};
+
+
+//Find all the restaurants from given city(pass in the city name, e.g. "Glasgow" should return 4 restaurants)
+ScranAdvisor.prototype.
+
 
 module.exports = ScranAdvisor;
